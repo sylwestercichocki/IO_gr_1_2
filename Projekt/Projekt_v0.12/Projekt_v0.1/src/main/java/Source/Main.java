9c8adfd2557d2c3268ed1ae45c76b37481f4aa62
marekgre <greq94@gmail.com>
2017-01-23 23:12:41 +0100

    Najnowasza wersja projektu

---
+++ Projekt/Projekt_v0.12/Projekt_v0.1/src/main/java/Source/Main.java
@@ -0,0 +1,13 @@
+package main.java.Source;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        GrupyGUI grupyGUI = new GrupyGUI();
+
+    }
+}
